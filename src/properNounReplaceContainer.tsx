56f9b76a4e2ef579597e2054f9a3e997a1ac2f76
unknown <zachary.romick@vanderbilt.edu>
2020-09-03 14:26:42 -0500

    Added English contractions to excluded word list

--- src/properNounReplaceContainer.tsx
+++ src/properNounReplaceContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ProperNounReplace from './properNounReplace';
-import { ospd } from './ospd'; // Official Scrabble Player's Dictionary
+import { ospd, wikiContractions } from './commonWords'; // Official Scrabble Player's Dictionary
 import _ from 'lodash';
 import $ from 'jquery';
 import { WordWithContextModel } from './models';
@@ -13,18 +13,20 @@ const ProperNounReplaceContainer = () => {
 	const sortWords = () => {
 		let excludedWords: WordWithContextModel[] = [];
 		let includedWords: WordWithContextModel[] = [];
-		// const excludedWordsElement = document.getElementById("excludedWords") as HTMLInputElement;
-		// const includedWordsElement = document.getElementById("includedWords") as HTMLInputElement;
 		const userTextArea = document.getElementById("userTextArea") as HTMLTextAreaElement;
-		const scrabbleDictionary = ospd();
+		// Default exlusions include English Scrabble words and English contractions.
+		let defaultExcludedWords = ospd().concat(wikiContractions().map(contraction => contraction.toUpperCase()));
+
+		// Add "I" and "A" to excluded words.
+		// defaultExcludedWords = defaultExcludedWords.concat(["A", "I"]);
 
 		// Tallying removed for now
-		// Tally the total number of 
+		// Tally the total number of
 		// const talliedWords = _.countBy(words, _.identity);
 
 		if (userTextArea) {
-			// Split all user text into an array when hitting white space and line breaks.
-			const allWords = userTextArea.value.split(/\s+/);
+			// Split all user text into an array when hitting white spac, line breaks, and dashes.
+			const allWords = userTextArea.value.split(/\s+|-/);
 			// Add context to the words by getting surrounding words
 			const allWordsWithContext = allWords.map((word, i) => {
 				const contextString =
@@ -34,21 +36,19 @@ const ProperNounReplaceContainer = () => {
 				return { [word]: { contextString, wordLocation, wordLength } }
 			});
 			console.log('all words', allWordsWithContext);
-			// Clean and standardize words and then see if they are in OSPD.
-			// If the word is in the OSPD, exclude the word.
+			// Clean and standardize words and then check if they are in OSPD.
 			allWordsWithContext.map(wordWithContext => {
-				// Test if word only contains a number
-				if (!/^\d+$/.test(Object.keys(wordWithContext)[0])) {
-					// Rename keys to uppercase and without punctuation
-					const newKey = (Object.keys(wordWithContext)[0]).replace(/[^\w\s]/g, "").toUpperCase();
-					if (scrabbleDictionary.indexOf(newKey) !== -1) {
-						// excludedWords = _.unionBy(excludedWords, newKey); // Attempt to remove dupes
-						excludedWords.push({ [newKey]: Object.values(wordWithContext)[0] });
+				// Rename keys to uppercase and without punctuation (except apostrophes)
+				const newKey = (Object.keys(wordWithContext)[0]).replace(/[^\w\s']/g, "").toUpperCase();
+				// If the cleaned word is in the Scrabble Dictionary, exclude.
+				// If the uncleaned word contains a number, exclude.
+				if (defaultExcludedWords.indexOf(newKey) !== -1 || /\d/.test(Object.keys(wordWithContext)[0])) {
+					// excludedWords = _.unionBy(excludedWords, newKey); // Attempt to remove dupes
+					excludedWords.push({ [newKey]: Object.values(wordWithContext)[0] });
 
-					} else {
-						// includedWords = _.unionBy(includedWords, newKey); // Attempt to remove dupes
-						includedWords.push({ [newKey]: Object.values(wordWithContext)[0] });
-					}
+				} else {
+					// includedWords = _.unionBy(includedWords, newKey); // Attempt to remove dupes
+					includedWords.push({ [newKey]: Object.values(wordWithContext)[0] });
 				}
 				return null;
 			})
